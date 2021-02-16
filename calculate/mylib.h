f5addc599d7cd8cfdc0106bba5952c2e97fb8803
Jon <beznosovevgenii@mail.ru>
2021-02-16 19:47:07 +0700

    add calculate

---
+++ calculate/mylib.h
@@ -0,0 +1,12 @@
+#ifndef __MYLYB__
+#define __sum__
+#define __dif__
+#define __multiply__
+#define __split__
+
+double sum(double value_1, double value_2, double *result);
+double dif(double value_1, double value_2, double *result);
+double multiply(double value_1, double value_2, double *result);
+double split(double value_1, double value_2, double *result);
+
+#endif
---
+++ calculate/split.c
@@ -0,0 +1,8 @@
+#include "mylib.h"
+#include <stdio.h>
+
+double split(double value_1, double value_2, double *result)
+{
+        *result = value_1 / value_2;
+        return 0;
+}

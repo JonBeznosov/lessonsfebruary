49fde2b1fabe66b39d3547ea92c0dcf35922aefb
Jon <beznosovevgenii@mail.ru>
2021-02-22 15:58:00 +0700

    работа с библиотекой

---
+++ calculate/lib.c
@@ -0,0 +1,27 @@
+double sum(double x, double y)
+{
+	return x + y;
+
+};
+
+double split(double x, double y)
+{
+        return x / y;
+
+};
+
+double multiply(double x, double y)
+{
+        return x * y;
+
+};
+
+double dif(double x, double y)
+{
+        return x - y;
+};
+
+/*double power(double x)
+{
+	return x*x;
+};*/
--- calculate/main.c
+++ calculate/main.c
@@ -1,11 +1,12 @@
-#include "mylib.h"
 #include <stdio.h>
-#include <math.h>
 
-int main() 
-{
-	double value_1, value_2, result;
-	char symbol[2];
+/* заголовочный файл для работы с динамическими библиотеками */
+#include <dlfcn.h>
+
+int main(int argc, char* argv[]){
+
+	void *ext_library;	// хандлер внешней библиотеки
+	double value_1,value_2;
 
 	printf("Input value_1 = ");
 	scanf("%lf", &value_1);
@@ -13,27 +14,23 @@ int main()
 	printf("Input value_2 = ");
 	scanf("%lf", &value_2);
 
-	printf("Input operation: +, -, *, /\n");
-	scanf("%s", symbol);
+	double (*powerfunc)(double x, double y);
 
-	if (symbol[0] == '+')
-	{
-		sum(value_1,value_2,&result);
-	}
-	else if (symbol[0] == '-')
-	{
-		dif(value_1,value_2,&result);
-	}
-	else if (symbol[0] == '*')
-	{
-		multiply(value_1,value_2,&result);
-	}
-	else
-	{
-		split(value_1,value_2,&result);
-	}
+	//загрузка библиотеки
+	ext_library = dlopen("/home/jw/Рабочий стол/lessonfebruary/calculate/libpowers.so",RTLD_LAZY);
+	if (!ext_library){
+		//если ошибка, то вывести ее на экран
+		fprintf(stderr,"dlopen() error: %s\n", dlerror());
+		return 1;
+	};
 
-	printf("Result = %1.2lf\n", result);
+	//загружаем из библиотеки требуемую процедуру
+	powerfunc = dlsym(ext_library, argv[1]);
 
-	return 0;
-}
+
+	//выводим результат работы процедуры
+	printf("%s(%1.2lf,%1.2lf) = %1.2lf\n",argv[1],value_1,value_2,(*powerfunc)(value_1,value_2));
+
+	//закрываем библиотеку
+	dlclose(ext_library);
+};

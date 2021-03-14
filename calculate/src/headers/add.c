d8c61cd3a8b9e03897e06f293d4fec3f0b266381
Jon <beznosovevgenii@mail.ru>
2021-03-14 22:12:59 +0700

    add calculate

---
+++ calculate/src/headers/add.c
@@ -0,0 +1,7 @@
+char func_name[] = "add";
+
+double add(double x, double y)
+{
+	return x + y;
+
+};
---
+++ calculate/src/headers/sub.c
@@ -0,0 +1,6 @@
+char func_name[] = "sub";
+
+double sub(double x, double y)
+{
+        return x - y;
+};

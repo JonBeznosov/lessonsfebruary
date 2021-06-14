6258e84c00d07fcbdb9b9938566a15db5e56df49
Jon <beznosovevgenii@mail.ru>
2021-06-14 11:39:03 +0700

    change client.c, server.c

--- ELTEX/udp_client_server/client.c
+++ ELTEX/udp_client_server/client.c
@@ -3,17 +3,17 @@
 int main(int argc, char *argv[])
 {
     struct sockaddr_in  svaddr, claddr;
-    int fd;
+    int sfd;
     size_t msg_len;   
     ssize_t num_bytes;           
     char buf[BUF_SIZE];
 
-    fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (fd == -1)
+    sfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sfd == -1)
     {
         handle_error("socket");
     }
-
+    
     memset(&svaddr, 0, sizeof(struct sockaddr_in));
     svaddr.sin_family = AF_INET;
     inet_pton(AF_INET, SERVER_ADDR, &(svaddr.sin_addr));
@@ -28,23 +28,24 @@ int main(int argc, char *argv[])
         msg_len = strlen(buf);
         buf[msg_len - 1] = '\0';
 
-        if (sendto(fd, buf, msg_len, 0, (struct sockaddr *) &svaddr, sizeof(struct sockaddr_in)) != msg_len);
+        if (sendto(sfd, buf, msg_len, 0, (struct sockaddr *) &svaddr, 
+                  sizeof(struct sockaddr_in)) != msg_len)
         {
             handle_error("sendto");
-        }
-        
-        num_bytes = recvfrom(fd, buf, BUF_SIZE, 0, NULL, NULL);
+	}
+	
+        num_bytes = recvfrom(sfd, buf, BUF_SIZE, 0, NULL, NULL);
         if (num_bytes == -1)
         {
             handle_error("recvfrom");
-        }
+	}
         printf("Message from server: %s\n", buf);
     }
 
-    if (close(fd) == -1)
+    if (close(sfd) == -1)
     {
         handle_error("close");
     }
-
+    
     return 0;
 }

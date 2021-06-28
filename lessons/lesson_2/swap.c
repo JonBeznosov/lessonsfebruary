ce7923cc2d89fb1d775e57a2107244d5d32bb2af
Jon <beznosovevgenii@mail.ru>
2021-06-28 20:16:15 +0700

    add lesson_2 lesson_3

---
+++ lessons/lesson_2/swap.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int swap(
+    int *a, 
+    int *b) 
+{
+
+    int temp;
+    printf("adress in swap - %p\n", a);
+    printf("value a = %d\n", *a);   
+  
+    temp = *b;        
+    *b = *a;
+    *a = temp;
+}
+
+
+int main(void){
+    int a = 2;
+    int b = 1;
+
+
+    printf("in main a = %d, b = %d\n", a, b);
+    printf("adress in main - %p\n", &a);  
+    
+    swap(&a, &b);      
+    
+    printf("in main a = %d, b = %d\n", a, b);
+    
+    return 0;
+}
--- lessons/lesson_3/square.c
+++ lessons/lesson_3/square.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-double square(double a, double b, double c, double *x1, double *x2)
+double square(
+  double a, 
+  double b, 
+  double c, 
+  double *x1, 
+  double *x2)
 {
     double d = (pow(b, 2)) - (4 * a * c);
 
     if (d > 0)
     {
- 	*x1 = (-b) - sqrt(d) / (2 * a);
- 	*x2 = (-b) + sqrt(d) / (2 * a);
+       	*x1 = (-b) - sqrt(d) / (2 * a);
+       	*x2 = (-b) + sqrt(d) / (2 * a);
     }
     else if (d == 0)
     {

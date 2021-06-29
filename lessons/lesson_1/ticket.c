ab05abd46a573e5e683a3014a77fb2802a9a1e72
Jon <beznosovevgenii@mail.ru>
2021-06-29 23:13:43 +0700

    fixed ticket.c

--- lessons/lesson_1/ticket.c
+++ lessons/lesson_1/ticket.c
@@ -1,47 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #define N 3
+#define BUF_SIZE 100
+#define LENGTH_NUMBER 6
 
-int summa_number(
-    int *number,
-    int *sum1, 
-    int *sum2)
+void summa_number(int number) 
 {
+    int sum1 = 0; 
+    int sum2 = 0;   
 
     for (int i = 0; i < N; i++)
     {
-    	*sum1 += (*number % 10);
-    	*number /= 10;
+    	sum1 += (number % 10);
+    	number /= 10;
     }
 
     for (int i = 0; i < N; i++)
     {
-    	*sum2 += (*number % 10);
-    	*number /= 10;
+    	sum2 += (number % 10);
+    	number /= 10;
     }
-
-    return 0;
-
-}
-
-int main(void) {
-	int number = 0;
-	int sum1 = 0; 
-	int sum2 = 0; 	
-	
-	printf("enter a six-digit number\n");
-	scanf("%d", &number);
-
-	summa_number(&number, &sum1, &sum2);
 
     if(sum1 == sum2)
-    {	
-       	printf("lucky ticket\n");
+    {   
+        printf("This is a lucky ticket!\n");
     }
     else
     {
-    	printf("an unlucky ticket\n");
+        printf("This is not a lucky ticket.\n");
+    }
+}
+
+int check_number(char *str)
+{
+    int is_number = 1;
+     
+    str[strlen(str) - 1] = '\0';
+    
+    if (LENGTH_NUMBER == strlen(str))
+    {
+        for (int i = 0; i < strlen(str); i++)
+            if (!isdigit(str[i]))
+            {
+                printf("Please enter the numbers!\n");
+                is_number = 0;
+                break;
+            }
+    }
+    else
+    {
+        printf("Error, enter a six-digit number.\n");
+        is_number = 0;
+    }
+
+    return is_number;
+}
+
+int main(int argc, char *argv[]) {
+	
+    int number = 0;
+	
+    char str[BUF_SIZE] = {0};
+
+    printf("Enter a six-digit number.\n");
+    fgets(str, BUF_SIZE, stdin);
+
+    if(check_number(str))
+    {
+        number = atoi(str);
+        summa_number(number);
     }
     
     return 0;
